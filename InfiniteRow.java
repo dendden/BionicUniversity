47f17d85fa2a3438d4bf8dcd4e9d0e08c94f4e02
Denys Triasunov <dendden@users.noreply.github.com>
2016-01-29 15:34:01 +0200

    Added iterations counter for method myExp()

--- InfiniteRow.java
+++ InfiniteRow.java
@@ -3,21 +3,22 @@ import java.lang.Math;
 public class InfiniteRow {
 
 	public static void main(String[] args) {
-		System.out.println(myExp(63));
-		System.out.println(Math.exp(63));
+		System.out.println(myExp(1.0));
+		System.out.println(Math.exp(1.0));
 	}
 	
 	public static double myExp(double x) {
 		double result = 1.0;
 		double numerator = x;
 		double denominator = 1;
-		double fraction = numerator / denominator;
-		for ( int factorialBase = 1; fraction > 0; factorialBase++, numerator *= x ) {
+		double fraction = x;
+		int iterations = 0;
+		for ( int factorialBase = 1; fraction > 0; factorialBase++, numerator *= x, iterations++ ) {
 			denominator *= factorialBase;
 			fraction = numerator / denominator;
 			result += fraction;
 		}
-		
+		System.out.println("Total iterations = " + iterations);
 		return result;
 	}
 }
