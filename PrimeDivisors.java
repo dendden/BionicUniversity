e67868442f3706c328aa74b5207dc578aa661be2
Denys Triasunov <dendden@users.noreply.github.com>
2016-01-29 17:30:36 +0200

    Added check for whether given N is prime itself with immidiate exit

--- PrimeDivisors.java
+++ PrimeDivisors.java
@@ -2,7 +2,11 @@
 public class PrimeDivisors {
 
 	public static void main(String[] args) {
-		int n = 9999;
+		int n = 101;
+		if ( isPrime(n) ) {
+			System.out.println(n);
+			System.exit(0);
+		}
 		int halfN = n / 2;
 		for ( int i = 2; i < halfN; i++ ) {
 			if ( n % i == 0 && isPrime(i) ) {
