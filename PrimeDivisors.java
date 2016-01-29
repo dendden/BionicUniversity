d1cb9ec28d50f600ab57b6424d5d2bbccfb1d4e4
Denys Triasunov <dendden@users.noreply.github.com>
2016-01-29 17:07:45 +0200

    A program for finding all prime divisors of a given natural number N. Contains isPrime() method.

---
+++ PrimeDivisors.java
@@ -0,0 +1,31 @@
+
+public class PrimeDivisors {
+
+	public static void main(String[] args) {
+		int n = 9999;
+		int halfN = n / 2;
+		for ( int i = 2; i < halfN; i++ ) {
+			if ( n % i == 0 && isPrime(i) ) {
+				System.out.println(i);
+			}
+		}
+	}
+	
+	public static boolean isPrime(int num) {
+		if ( num < 2 ) {
+			return false;
+		}
+		if ( num == 2 ) {
+			return true;
+		}
+		if ( num % 2 == 0 ) {
+			return false;
+		}
+		for ( int i = 3; i * i <= num; i += 2 ) {
+			if ( num % i == 0 ) {
+				return false;
+			}
+		}
+		return true;
+	}
+}
