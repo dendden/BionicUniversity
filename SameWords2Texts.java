10b295dd65d0034e19817bc7667ac091a9070d9b
Denys Triasunov <dendden@users.noreply.github.com>
2016-02-10 19:46:25 +0200

    This upgraded version sorts both texts and compares only their unique words

--- SameWords2Texts.java
+++ SameWords2Texts.java
@@ -3,22 +3,32 @@ import java.util.Arrays;
 public class SameWords2Texts {
 
 	public static void main(String[] args) {
-		String text1 = "Hi: There?; me?";
-		String text2 = "Hello;.../ there, you and YOU/!";
+		String text1 = "Hi: I Say:There?; me...or you?";
+		String text2 = "Hello;.../ there, you and YOU/! I mean: 'HI!";
 		
-		System.out.println(numOfSameWords(text1,text2));
+		System.out.println("Total number of same words in 2 texts: " + numOfSameWords(text1,text2));
 	}
 	
 	public static int numOfSameWords(String s1, String s2) {		
-		String[] words1 = s1.split("\\W+");
-		String[] words2 = s2.split("\\W+");
+		String[] words1 = s1.toLowerCase().split("\\W+");
+		String[] words2 = s2.toLowerCase().split("\\W+");
+		Arrays.sort(words1);
+		Arrays.sort(words2);
+		int words1Len = words1.length;
+		int words2Len = words2.length;
 		int sameWords = 0;
 		
-		for ( String word1 : words1 ) {
-			for ( String word2 : words2 ) {
-				if ( word1.equalsIgnoreCase(word2) ) {
+		for ( int i = 0; i < words1Len; i++ ) {
+			if ( i > 0 && words1[i].equals(words1[i-1]) ) {
+				continue;
+			}
+			for ( int j = 0; j < words2Len; j++ ) {
+				if ( j > 0 && words2[j].equals(words2[j-1]) ) {
+					continue;
+				}
+				if ( words1[i].equals(words2[j]) ) {
 					sameWords += 1;
-					System.out.println("Same word: " + word1);
+					System.out.println("Same word: " + words1[i]);
 					break;
 				}
 			}
