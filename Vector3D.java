57d8a909c81f5e69b602694ad8f0b53aa65c92b9
Denys Triasunov <dendden@users.noreply.github.com>
2016-02-18 21:07:44 +0200

    addition and multiplication methods are now static and operate on two given Vector3D objects returning a resulting one

--- Vector3D.java
+++ Vector3D.java
@@ -14,6 +14,14 @@ public class Vector3D {
 	private double x2;
 	private double x3;
 	
+	public Vector3D(double x1, double x2, double x3) {
+		this.x1 = x1;
+		this.x2 = x2;
+		this.x3 = x3;
+	}
+	
+	public Vector3D() {}
+	
 	/**
 	 * Coordinates' getters and setters
 	 */
@@ -39,28 +47,37 @@ public class Vector3D {
 	/**
 	 * A method for adding another 3D vector to this
 	 */
-	public void add(Vector3D other) {
-		this.x1 += other.getX1();
-		this.x2 += other.getX2();
-		this.x3 += other.getX3();
+	public static Vector3D add(Vector3D v1, Vector3D v2) {
+		double x1 = v1.getX1() + v2.getX1();
+		double x2 = v1.getX2() + v2.getX2();
+		double x3 = v1.getX3() + v2.getX3();
+		
+		return new Vector3D(x1,x2,x3);
 	}
 	
 	/**
 	 * A scalar product of this and other 3D vector
 	 */
-	public void prodScalar(Vector3D other) {
-		this.x1 *= other.getX1();
-		this.x2 *= other.getX2();
-		this.x3 *= other.getX3();
+	public static Vector3D multiplyScalar(Vector3D v1, Vector3D v2) {
+		double x1 = v1.getX1() * v2.getX1();
+		double x2 = v1.getX2() * v2.getX2();
+		double x3 = v1.getX3() * v2.getX3();
+		
+		return new Vector3D(x1,x2,x3);
 	}
 	
 	/**
 	 * A vector product of this and other 3D vector
 	 */
-	public void prodVector(Vector3D other) {
-		this.x1 = this.x2 * other.getX3() - this.x3 * other.getX2();
-		this.x2 = this.x3 * other.getX1() - this.x1 * other.getX3();
-		this.x3 = this.x1 * other.getX2() - this.x2 * other.getX1();
+	public static Vector3D multiplyVector(Vector3D v1, Vector3D v2) {
+//		this.x1 = this.x2 * other.getX3() - this.x3 * other.getX2();
+//		this.x2 = this.x3 * other.getX1() - this.x1 * other.getX3();
+//		this.x3 = this.x1 * other.getX2() - this.x2 * other.getX1();
+		double x1 = v1.getX2() * v2.getX3() - v1.getX3() * v2.getX2();
+		double x2 = v1.getX3() * v2.getX1() - v1.getX1() * v2.getX3();
+		double x3 = v1.getX1() * v2.getX2() - v1.getX2() * v2.getX1();
+		
+		return new Vector3D(x1,x2,x3);
 	}
 	
 	/**
